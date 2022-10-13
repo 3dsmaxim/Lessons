d1f8805e28eaaf0cb63a9a9df0bcd0a94f9c01c6
Maxim Stepanyuk <3dsmaxim@mail.ru>
2022-10-13 21:44:23 +0300

    lessons001 1step

--- Lesson001/Program.cs
+++ Lesson001/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sqrt = number * number;
+
+
+
+
+
+Console.WriteLine("Число возведенное в квадлрат: " + sqrt);
